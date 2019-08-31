79ae12c2d3b6ed46d7f9022e1aae1e1afe389d8d
workingdog <ringowathelet@gmail.com>
2019-08-31 22:48:11 +0900

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Ringo Wathelet on 2019/08/31.
+//  Copyright © 2019 Ringo Wathelet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
