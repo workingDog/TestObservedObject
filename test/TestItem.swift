96b0b480bb5f067124b7e6877ffd6271cdcd1bbd
workingdog <ringowathelet@gmail.com>
2019-09-01 09:39:12 +0900

    initial commit

--- test/TestItem.swift
+++ test/TestItem.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TestItem: Identifiable, Equatable {
+    
+    var id = UUID()
+    
+    init(text: String) {
+        self.text = text
+        self.isRead = false
+    }
+    
+    static func == (lhs: TestItem, rhs: TestItem) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    var text: String
+    var isRead: Bool
+    var read: Bool {
+        set { self.isRead = newValue }
+        get { self.isRead }
+    }
+}
